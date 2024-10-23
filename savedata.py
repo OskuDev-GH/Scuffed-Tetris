4ac072484244b3b2dc4cfce441e4369608c81a55
OskuDev <98336719+OskuDev-GH@users.noreply.github.com>
2024-10-23 22:47:23 +0300

    Add files via upload

--- savedata.py
+++ savedata.py
@@ -10,15 +10,17 @@ class SaveData:
 
     def save(self, score):
         with open(os.path.join(self.save_file_path, self.save_file_name), "w") as file:
-            file.write(self.cipher("score: " + str(score)))
+            file.write(self.cipher("score:" + str(score)))
 
     def load(self):
         try:
             with open(os.path.join(self.save_file_path, self.save_file_name), "r") as file:
                 encrypted_score = file.read()
-                return int(self.decipher(encrypted_score).lstrip(':'))
-        except FileNotFoundError:
+                decrypted_score = self.decipher(encrypted_score)
+                return int(decrypted_score.split(':')[1])
+        except (FileNotFoundError, IndexError, ValueError):
             return 0
+
 
     def cipher(self, text):
         result = ""
